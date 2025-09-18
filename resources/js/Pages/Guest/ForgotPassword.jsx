99c7f20a46f0d5dd27b24b1ec39feebc7fc1c918
welsondeivid <53789831+welsondeivid@users.noreply.github.com>
2025-09-18 10:11:53 -0300

    :construction: feat: Tela de login

---
+++ resources/js/Pages/Guest/ForgotPassword.jsx
@@ -0,0 +1,6 @@
+
+export default function ForgotPassword(){
+    return(
+        <p>TODO: Tela para Recuperar Senha</p>
+    );
+};
--- resources/js/Pages/Guest/Login.jsx
+++ resources/js/Pages/Guest/Login.jsx
@@ -1,5 +1,10 @@
 import { useForm } from '@inertiajs/react'
+import { Link } from '@inertiajs/react'
 import { route } from 'ziggy-js'
+import EmailInput from '../../Components/EmailInput'
+import PasswordInput from '../../Components/PasswordInput'
+import Button from '../../Components/Button'
+import Background from '../Assets/background.svg'
 
 
 export default function Login() {
@@ -9,29 +14,68 @@ export default function Login() {
     remember: false,
   })
 
-  const submit = (e) => {
+  const submitLogin = (e) => {
     e.preventDefault()
     post(route('authenticate'))
   }
 
   return (
-    <form onSubmit={submit}>
-      <input
-        type="email"
-        value={data.email}
-        onChange={(e) => setData('email', e.target.value)}
-      />
-      {errors.email && <div>{errors.email}</div>}
+    <div className='flex w-screen h-screen items-center justify-center'>
 
-      <input
-        type="password"
-        value={data.password}
-        onChange={(e) => setData('password', e.target.value)}
-      />
+      <div className='flex flex-col h-full w-1/2 items-center justify-around px-24'>
 
-      <button type="submit" disabled={processing}>
-        Entrar
-      </button>
+        <div className='flex w-full px-4 items-center justify-center'>
+          LOGO
+        </div>
+
+        <div className='flex flex-col gap-4 w-full px-4 items-center justify-center'>
+          <form onSubmit={submitLogin} className="flex flex-col gap-4 w-full">
+              <EmailInput
+                value={data.email}
+                onChange={(val) => setData('email', val)}
+                error={errors.email}
+              />
+
+              <PasswordInput
+                value={data.password}
+                onChange={(val) =>setData('password', val)}
+                error={errors.password}
+              />
+
+              <Button type="submit" disabled={processing} color={"blue"}>
+                {processing ? "Entrando..." : "Entrar"}
+              </Button>
+
+          </form>
+          <ResetPassword/>
+
+          <RegisterButton/>
+        </div>
+
+      </div>
+
+      <div className='flex w-1/2 h-full items-center justify-center'>
+        <Background></Background>
+      </div>
+
+    </div>
+  )
+}
+
+export function ResetPassword() {
+  return (
+    <Link href={route('forgot_password')} className="text-gray-600 text-center">
+      Esqueceu a senha?
+    </Link>
+  )
+}
+
+export function RegisterButton() {
+  return (
+    <form action={route('register')} className='w-full'>
+      <Button type="submit" color={"white"} className={"w-full"}>
+        Cadastro
+      </Button>
     </form>
   )
 }
