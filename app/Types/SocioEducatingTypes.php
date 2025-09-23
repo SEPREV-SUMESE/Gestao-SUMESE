4d6faa7ac7fdbb0eaa175f91cd6d28421091c6da
Davi Souza <dsl@ic.ufal.br>
2025-09-23 10:07:33 -0300

    :construction: raw: migrations

---
+++ app/Types/SocioEducatingTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Types;
+
+class SocioEducatingTypes extends Types
+{
+    public const MARITAL_STATUS = ['SOLTEIRO', 'CASADO', 'VIUVO', 'DIVORCIADO'];
+    public const GENDER_IDENTITY = [];
+    public const SEXUAL_ORIENTATION = [];
+    public const RACE = [];
+    public const EDUCATION_LEVEL = [];
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email', 55)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_admin')->default(false);
+            $table->foreignId('role_id')->constrained('roles')->onDelete('set null')->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
