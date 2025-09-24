7867402a9616ba89f82c4c6ed6f98f9252480157
Davi Souza <dsl@ic.ufal.br>
2025-09-24 13:37:30 -0300

    :construction: login

--- resources/js/Pages/Guest/Login.jsx
+++ resources/js/Pages/Guest/Login.jsx
@@ -4,7 +4,7 @@ import { route } from 'ziggy-js'
 import EmailInput from '../../Components/EmailInput'
 import PasswordInput from '../../Components/PasswordInput'
 import Button from '../../Components/Button'
-import Background from '../Assets/background.svg'
+import LoginBackground from '../../Components/LoginBackground'
 
 
 export default function Login() {
@@ -20,44 +20,46 @@ export default function Login() {
   }
 
   return (
-    <div className='flex w-screen h-screen items-center justify-center'>
+    <div className='flex w-screen h-screen items-center justify-center bg-gray-500'>
 
-      <div className='flex flex-col h-full w-1/2 items-center justify-around px-24'>
+      <div className='flex w-[50%] h-[50%] p-4 bg-red-200'>
 
-        <div className='flex w-full px-4 items-center justify-center'>
-          LOGO
+        <div className='flex flex-col h-full w-full items-center justify-center gap-10 px-24 bg-green-300'>
+
+          <div className='flex w-full px-4 items-center justify-center'>
+            LOGO
+          </div>
+
+          <div className='flex flex-col gap-4 w-full px-4 items-center justify-center'>
+            <form onSubmit={submitLogin} className="flex flex-col gap-4 w-full">
+                <EmailInput
+                  value={data.email}
+                  onChange={(val) => setData('email', val)}
+                  error={errors.email}
+                />
+
+                <PasswordInput
+                  value={data.password}
+                  onChange={(val) =>setData('password', val)}
+                  error={errors.password}
+                />
+
+                <Button type="submit" disabled={processing} color={"blue"}>
+                  {processing ? "Entrando..." : "Entrar"}
+                </Button>
+
+            </form>
+            <ResetPassword/>
+
+            <RegisterButton/>
+          </div>
+
         </div>
 
-        <div className='flex flex-col gap-4 w-full px-4 items-center justify-center'>
-          <form onSubmit={submitLogin} className="flex flex-col gap-4 w-full">
-              <EmailInput
-                value={data.email}
-                onChange={(val) => setData('email', val)}
-                error={errors.email}
-              />
-
-              <PasswordInput
-                value={data.password}
-                onChange={(val) =>setData('password', val)}
-                error={errors.password}
-              />
-
-              <Button type="submit" disabled={processing} color={"blue"}>
-                {processing ? "Entrando..." : "Entrar"}
-              </Button>
-
-          </form>
-          <ResetPassword/>
-
-          <RegisterButton/>
+        <div className='flex w-full h-full items-center justify-center object-cover'>
+          <LoginBackground/>
         </div>
-
       </div>
-
-      <div className='flex w-1/2 h-full items-center justify-center'>
-        <Background></Background>
-      </div>
-
     </div>
   )
 }
